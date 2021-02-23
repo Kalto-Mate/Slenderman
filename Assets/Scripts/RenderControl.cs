e266d2036f888006635072873979438e36398fb1
Kalto-Mate <Guillermo_Gonzalez_Sanchez@outlook.com>
2021-02-23 20:55:44 +0100

    Class commit

---
+++ Assets/Scripts/RenderControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RenderControl : MonoBehaviour
+{
+    public Material material;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, material);
+    }
+}
